36f3c2fc885ba14f7802e1217928e21a6560cc1f
leeyoshinari <lee_jc@outlook.com>
2025-03-21 19:42:15 +0800

    add cmd

--- startup.sh
+++ startup.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 ip=$(cat .env | grep -E "^winHubHost" | awk -F '=' '{print $2}' | awk -F '\r' '{print $1}' | tr -d '[:space:]')
 port=$(cat .env | grep -E "^winHubPort" | awk -F '=' '{print $2}' | awk -F '\r' '{print $1}' | tr -d '[:space:]')
-gunicorn main:app -b $ip:$port -k uvicorn.workers.UvicornWorker --timeout 30 --daemon
+gcmd=$(cat .env | grep -E "^winHubGunicornCmd" | awk -F '=' '{print $2}' | awk -F '\r' '{print $1}' | tr -d '[:space:]')
+$gcmd main:app -b $ip:$port -k uvicorn.workers.UvicornWorker --timeout 30 --daemon
 echo "start server success ~"
